0500f15663a9e13edbfda07baf60c99fb00b0bc8
mikebrucato <brucatomike1@gmail.com>
2021-02-22 16:08:10 -0600

    add header

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default {
-
+    // random user API request with axios
     getUsers: function() {
         return axios.get("https://randomuser.me/api/?results=200&nat=us")
     }
